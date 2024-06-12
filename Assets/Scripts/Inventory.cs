1a2ccf5b87412aeafac32936de841e45208fe4e8
Nicolas Pires Simão <rm90173@estudante.fieb.edu.br>
2024-06-12 13:25:51 -0300

    inventario_drop_error

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Inventory : MonoBehaviour
 {
     public GameObject mouseItem;
-   
+    public Item[] item;
 
     public void DragItem( GameObject button )
     {
@@ -15,8 +16,19 @@ public class Inventory : MonoBehaviour
 
     public void DropItem( GameObject button)
     {
-        Transform aux = mouseItem.transform.parent;
-        mouseItem.transform.SetParent(button.transform.parent);
-        button.transform.SetParent(aux);
+        if(mouseItem != null)
+        {
+            if(button.name.Equals("Drop"))
+            {
+                int pos = int.Parse(mouseItem.name);
+                Instantiate(item[pos].prefab, Vector3.zero, Quaternion.identity);
+            }
+            else
+            {
+                Transform aux = mouseItem.transform.parent;
+                mouseItem.transform.SetParent(button.transform.parent);
+                button.transform.SetParent(aux);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item 
+{
+    public string id;
+    public int iAttack;
+    public Sprite sprite;
+    public Transform prefab;
+}
